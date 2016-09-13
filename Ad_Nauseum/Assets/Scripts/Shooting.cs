6b912e1131a9a6c2f88fee5118ffb9c7bb51de7a
Jordan Gagnon <jrgagnon@mtu.edu>
2016-09-13 19:55:50 -0400

    Added Shooting

---
+++ Ad_Nauseum/Assets/Scripts/Shooting.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shooting : MonoBehaviour {
+
+	public GameObject bullet;
+	private Rigidbody2D self;
+	// Use this for initialization
+	void Start () {
+		self = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire2")) {
+			Instantiate (bullet, self.position, Quaternion.identity);
+		}
+			
+	}
+}
