516315df715a741d32fb3190f4c3f4606c9d00cd
Zachary Paris <zgparis@mtu.edu>
2016-09-13 19:58:11 -0400

    Added Death Trigger

---
+++ Ad_Nauseum/Assets/Scripts/DeathCollsion.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeathCollsion : MonoBehaviour {
+
+	//For the player's own collider
+	private BoxCollider2D self;
+
+	// Use this for initialization
+	void Start () {
+		//Find the player's own collider
+		self = GetComponent<BoxCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	//Called when the player collides with another trigger
+	void OnTriggerEnter2D(Collider2D other) {
+		
+		//Checks the other object's tag: if it's the tag we're looking for, continue
+		if (other.gameObject.CompareTag("EvilTrigger")) {
+			gameObject.SetActive (false);
+		}
+	}
+}
