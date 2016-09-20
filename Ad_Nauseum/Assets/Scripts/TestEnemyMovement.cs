7edcff0a718c2a60f0ec7da9206bd20c36e456cd
Zachary Paris <zgparis@mtu.edu>
2016-09-20 18:36:29 -0400

    Working on enemy movement

---
+++ Ad_Nauseum/Assets/Scripts/TestEnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestEnemyMovement : MonoBehaviour {
+
+	Rigidbody2D body;
+	Rigidbody2D player;
+
+	// Use this for initialization
+	void Start () {
+		body = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+
+
+	}
+}
