91a2257387418912fdf3802e83dbd246eba11820
Jordan Gagnon <jrgagnon@mtu.edu>
2016-09-28 19:37:40 -0400

    Added animations

--- Ad_Nauseum/Assets/Scripts/Shooting.cs
+++ Ad_Nauseum/Assets/Scripts/Shooting.cs
@@ -7,21 +7,37 @@ public class Shooting : MonoBehaviour {
 	public float bulletAdX;
 	public float bulletAdY;
 	private Rigidbody2D self;
+	private float shootPause;
+
+	private Animator animator;
 
 	// Use this for initialization
 	void Start () {
 		self = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+		shootPause = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetButtonDown ("Fire2")) {
 			Instantiate (bullet, new Vector2(self.position.x + bulletAdX, self.position.y + bulletAdY), Quaternion.identity);
-			GetComponent<Animator>().SetBool("shooting", true);
+
+			//animator.SetTrigger ("shooting");
+			animator.SetBool("shooting", true);
+			shootPause = 1f;
+
 		}
+		if (shootPause <= 0) {
+			animator.SetBool ("shooting", false);
+		} else {
+			shootPause -= 1 * Time.deltaTime;
+		}
+
 			
 
 	}
+
 
 
 }
