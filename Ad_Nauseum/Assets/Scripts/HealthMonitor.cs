2a6aaf1f226a0e964a9a5573595cb12d4425004a
Brett Kriz <bakriz@mtu.edu>
2016-10-12 20:49:16 -0400

    Added 3 scripts, CameraFollow, HealthMonitor, Session_Monitor
    These scripts are ready to use

---
+++ Ad_Nauseum/Assets/Scripts/HealthMonitor.cs
@@ -0,0 +1,131 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthMonitor : MonoBehaviour
+{
+
+    public int HP = 3;
+    public int MaxHP = 5;
+
+    public bool DEBUG_Bleedout_on = false;
+
+    public GameObject Player;
+    public Collider2D Collison;
+    public Rigidbody2D RBody;
+    public Session_Monitor Ses; // Optional
+
+    // Use this for initialization
+    void Start()
+    {
+        Ses = Session_Monitor.Instance;
+        {
+            // If the starting HP is not set, set as total
+            // So people dont die RIGHT at start
+            this.HP += this.MaxHP;
+
+            if (Player == null)
+            {
+                Debug.Log("[!] Player gameobj NOT linked in HEALTH MONITOR!  Please attach object or talk to Brett!");
+            }
+            if (Collison == null)
+            {
+                Collison = Player.GetComponent<Collider2D>();
+            }
+            if (RBody == null)
+            {
+                RBody = Player.GetComponent<Rigidbody2D>();
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //t.text = "Hp: " + HP + "\nStamina: " + Stamina + "\nFuel:" + Flame;
+        if (this.IsDead)
+        {
+            this.Ses.End();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // In addition to physics...
+        // Fixed interval regen will be handled here
+        if (DEBUG_Bleedout_on == true)
+        {
+            this.HP -= 1;
+            //Debug.Log("[i] Bleeding... -1 -> "+this.HP);
+        }
+    }
+
+    public bool IsAlive
+    { // Attribute
+        get { return this.HP > 0; }
+    }
+    public bool IsDead
+    {
+        get { return !this.IsAlive; }
+    }
+
+    public void ChangeHP(int dmg)
+    {
+
+        // You will notice, the damage is added...
+        this.HP -= dmg;
+
+        // Check to make sure we didnt just die lol
+
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        //int dmg = 0; // coll.gameObject.getDamage
+        //dmg = (int)Mathf.Abs(this.RBody.velocity.magnitude);
+
+        // Please make these globals!!!
+        bool IsEnemy = coll.gameObject.tag == "Enemy";
+        bool IsItem = coll.gameObject.tag == "Item";
+        bool IsWorld = coll.gameObject.tag == "World";
+
+        //bool IsOtherDead = (dmg > 5);
+        GameObject Other = coll.gameObject;
+
+        // coll.gameObject
+        // @@@Need flag based on what we hit...
+        /*
+        if (IsEnemy)
+        {
+            // Its an enemy, so we'll take damage.
+            // Damage is NEGATED for simplicity here!!!
+            ChangeHP(dmg);
+            if (IsOtherDead)
+            {
+                Destroy(Other); // Check me
+            }
+
+        }else */
+
+        if (IsItem)
+        {
+            // @@@
+
+        }
+    }
+
+    void OnCollisionExit2D(Collider coll)
+    {
+        bool IsEnemy = coll.gameObject.tag == "Enemy";
+        bool IsItem = coll.gameObject.tag == "Item";
+        bool IsWorld = coll.gameObject.tag == "World";
+
+        if (IsEnemy || IsWorld)
+        {
+            // Because you cant just take damage on ground
+            Debug.Log("Off ground");
+        }
+    }
+// END OF FILE
+}
+// Nothing
